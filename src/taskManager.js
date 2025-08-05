d2a77c7480a8ef6617d802475b2ab58a63f09e4c
Koteswararao <neerikshan02@gmail.com>
2025-08-05 16:15:40 +0530

    feat: add completeTask and getTasks methods

--- src/taskManager.js
+++ src/taskManager.js
@@ -13,3 +13,16 @@ class TaskManager {
     }
 }
 module.exports = TaskManager;
+
+    completeTask(id) {
+        const task = this.tasks.find(t => t.id === id);
+        if (task) {
+            task.completed = true;
+            return true;
+        }
+        return false;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
